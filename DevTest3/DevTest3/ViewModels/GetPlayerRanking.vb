f25fe1fc34e185676ec1b8a91d5fdfd24161e913
dnetdevone <net_dev@live.com>
2018-08-18 16:17:36 -0500

    Initializing the value for the view model
    we are setting an initial value for the view model so that we know what to expect on the front end

--- DevTest3/DevTest3/ViewModels/GetPlayerRanking.vb
+++ DevTest3/DevTest3/ViewModels/GetPlayerRanking.vb
@@ -1,7 +1,13 @@
-﻿
+
 Public Class GetPlayerRanking
 
     Property Name As String
     Property Ranking As Integer
+
+    Public Sub New()
+        Name = ""
+        Ranking = 0
+    End Sub
+
 
 End Class
